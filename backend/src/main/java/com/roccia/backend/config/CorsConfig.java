a2243145b010da53dabf5b94241ea56edc0e8106
rmsckd1640 <rmsckd1640@gmail.com>
2026-07-16 05:04:50 +0900

    fix: 불필요하게 추가돼있던 운영 환경 CORS 설정 제거

--- backend/src/main/java/com/roccia/backend/config/CorsConfig.java
+++ backend/src/main/java/com/roccia/backend/config/CorsConfig.java
@@ -2,12 +2,14 @@ package com.roccia.backend.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@Profile("!prod")
 public class CorsConfig implements WebMvcConfigurer {
 
     private final List<String> allowedOriginPatterns;
